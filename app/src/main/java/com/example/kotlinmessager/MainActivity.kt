fff6f822763f34de69c44d51ecb29f2c95aab518
anchieta <anchieta.gm@gmail.com>
2019-05-23 01:51:36 -0300

    MainActivit semi pronta

--- app/src/main/java/com/example/kotlinmessager/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessager/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.kotlinmessager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        gravar_button.setOnClickListener {
+            val nome = nome_editText.text.toString()
+            val email = email_editText.text.toString()
+            val senha = senha_editText.text.toString()
+
+            Log.d( "MainActivity", "Nome: $nome")
+            Log.d("MainActivity", "Email: " + email)
+            Log.d( "MainActivity",  "Senha: $senha")
+        } //btn
+
+        ja_tem_conta_textView.setOnClickListener {
+            Log.d("MainActivity", "Tente ver a tela de login")
+        } //texto click
+
+    } //onCreate
+} //class
