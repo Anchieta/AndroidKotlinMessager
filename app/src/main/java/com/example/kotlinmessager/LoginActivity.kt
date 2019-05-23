0e3db47fe636f192bf779e26407210210cb7a40b
anchieta <anchieta.gm@gmail.com>
2019-05-23 16:08:07 -0300

    Tela de Login funcionalidades básicas

--- app/src/main/java/com/example/kotlinmessager/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessager/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlinmessager
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
@@ -11,6 +13,19 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
-    }
 
-}
+        login_button.setOnClickListener {
+            val email = email_editText.text.toString()
+            val senha = senha_editText.text.toString()
+
+            Log.d("Login", "Email: $email e Senha: $senha")
+        } //login
+
+        voltar_textView.setOnClickListener {
+            finish()
+        }
+
+
+    } //onCreate
+
+}//class
