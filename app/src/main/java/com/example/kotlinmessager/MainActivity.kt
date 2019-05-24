a916ce60b31567466c12c09503fc2f40109c2ee1
anchieta <anchieta.gm@gmail.com>
2019-05-23 23:12:09 -0300

    Código na MainAction para autenticação - NÃO ESTA FUNCIONANDO

--- app/src/main/java/com/example/kotlinmessager/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessager/MainActivity.kt
@@ -4,13 +4,25 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        //updateUI(currentUser)
 
         gravar_button.setOnClickListener {
             val nome = nome_editText.text.toString()
@@ -20,6 +32,42 @@ class MainActivity : AppCompatActivity() {
             Log.d( "MainActivity", "Nome: $nome")
             Log.d("MainActivity", "Email: " + email)
             Log.d( "MainActivity",  "Senha: $senha")
+
+            //Autenticando no Firebase
+            /*
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    Log.d("Main", "Erro!!!!")
+
+                    return@addOnCompleteListener
+                }
+
+
+                //else - Se for sucesso
+                Log.d("Main", "Usuário criado com sucesso!\n\nuid: ${it.result?.user?.uid}")
+            }
+            */
+
+            auth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Main", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Main", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+
+                    // ...
+                }
+
+
+
         } //btn
 
         ja_tem_conta_textView.setOnClickListener {
