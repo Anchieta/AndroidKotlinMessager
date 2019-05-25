eab96f6fc126f668c190d441df53724bb48d5670
anchieta <anchieta.gm@gmail.com>
2019-05-25 19:03:55 -0300

    Botão ler/mostrar foto

--- app/src/main/java/com/example/kotlinmessager/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessager/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import android.widget.Toast.makeText
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.auth.FirebaseAuth
 
@@ -25,7 +26,14 @@ class MainActivity : AppCompatActivity() {
             //Executa a activity de login
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        } //ja_tem_conta
+
+        selectphoto_button.setOnClickListener {
+
+            Toast.makeText(this, "Tentando carregar photo", Toast.LENGTH_LONG).show()
+           // Log.d("MainAtivicty", "Tentando mostrar photo")
         }
+
     } //onCreate
 
     private fun performRegister() {
@@ -45,16 +53,17 @@ class MainActivity : AppCompatActivity() {
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
             .addOnCompleteListener {
                 if (!it.isSuccessful) {
-                    Toast.makeText(baseContext, "O registro não foi gravado!!!", Toast.LENGTH_LONG).show()
+                    makeText(baseContext, "O registro não foi gravado!!!", Toast.LENGTH_LONG).show()
                     return@addOnCompleteListener
                 }
                 //Else
                 Toast.makeText(baseContext, "Registro gravado com sucesso!!!", Toast.LENGTH_LONG).show()
             } //.addOnCompleteListener
             .addOnFailureListener {
-                Toast.makeText(baseContext, "Falha ao tentar criar o usuário. Erro: ${it.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(baseContext, "Falha ao tentar criar o usuário. Erro: ${it.message}", Toast.LENGTH_LONG).show()
             } //.addOnFailureListener
     }//performRegister
 
 
 } //class
+
